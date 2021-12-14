92a351aab2f18e0b6b61045148bf729f3884dc0a
sonangrai <shonahangrae@gmail.com>
2021-12-14 19:36:03 +0545

    styled ssr

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+/**
+ * This will help to render the styled component css in Server side
+ */
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
